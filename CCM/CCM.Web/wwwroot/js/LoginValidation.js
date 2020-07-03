a8b4f90995df90a9cdb2a08df305b364b12ad2a7
Niuniek <szymon.dolegowski@gamil.com>
2020-07-03 20:46:11 +0200

    Add validation for login and registration in GUI

---
+++ CCM/CCM.Web/wwwroot/js/LoginValidation.js
@@ -0,0 +1,32 @@
+﻿$('#loginform').submit(function () {
+    const username = $('#login-username').val();
+    const password = $('#login-password').val();
+
+    if (!username) {
+        turnOnIncorrectLoginInput('#login-username', '#usernameWarning');
+    }
+    else {
+        turnOffIncorrectLoginInput('#login-username', '#usernameWarning');
+    }
+
+    if (!password) {
+        turnOnIncorrectLoginInput('#login-password', '#passwordWarning');
+    }
+    else {
+        turnOffIncorrectLoginInput('#login-password', '#passwordWarning');
+    }
+
+    if (!username || !password) {
+        event.preventDefault();
+    }
+});
+
+function turnOnIncorrectLoginInput(inputName, warningMessage) {
+    $(inputName).attr('class', 'form-control is-invalid');
+    $(warningMessage).attr('style', 'display:block');
+}
+
+function turnOffIncorrectLoginInput(inputName, warningMessage) {
+    $(inputName).attr('class', 'form-control');
+    $(warningMessage).attr('style', 'display:none');
+}
--- CCM/CCM.Web/wwwroot/js/RegisterValidation.js
+++ CCM/CCM.Web/wwwroot/js/RegisterValidation.js
@@ -1,11 +1,98 @@
 ﻿$('#signupform').submit(function () {
+    const isRequiredFieldsEmpty = validateIsRequiredFieldsEmpty();
+    if (isRequiredFieldsEmpty) {
+        event.preventDefault();
+        return;
+    }
 
-   
-    var name = $.trim($('#log').val());
+    const isEmailValidate = validateIsEmailCorrect();
+    if (!isEmailValidate) {
+        event.preventDefault();
+        return;
+    }
 
-    
-    if (name === '') {
-        alert('Text-field is empty.');
-        return false;
+    const isPasswordConfirmedCorrect = validateIsPasswordConfirmedCorrect();
+    if (!isPasswordConfirmedCorrect) {
+        event.preventDefault();
     }
 });
+
+
+function validateIsRequiredFieldsEmpty() {
+    const username = $('#register-username').val();
+    const email = $('#register-email').val();
+    const password = $('#register-password').val();
+    const confirmpassword = $('#register-confirmpassword').val();
+
+    if (!username) {
+        turnOnIncorrectLoginInput('#register-username', '#usernameWarning');
+    }
+    else {
+        turnOffIncorrectLoginInput('#register-username', '#usernameWarning');
+    }
+
+    if (!email) {
+        turnOnIncorrectLoginInput('#register-email', '#emailWarning');
+    }
+    else {
+        turnOffIncorrectLoginInput('#register-email', '#emailWarning');
+    }
+
+    if (!password) {
+        turnOnIncorrectLoginInput('#register-password', '#passwordWarning');
+    }
+    else {
+        turnOffIncorrectLoginInput('#register-password', '#passwordWarning');
+    }
+
+    if (!confirmpassword) {
+        turnOnIncorrectLoginInput('#register-confirmpassword', '#confirmpasswordWarning');
+    }
+    else {
+        turnOffIncorrectLoginInput('#register-confirmpassword', '#confirmpasswordWarning');
+    }
+
+    if (!username ||
+        !email ||
+        !password ||
+        !confirmpassword) {
+
+        return true;
+    }
+    return false;
+}
+
+function turnOnIncorrectLoginInput(inputName, warningMessage) {
+    $(inputName).attr('class', 'form-control is-invalid');
+    $(warningMessage).attr('style', 'display:block');
+}
+
+function turnOffIncorrectLoginInput(inputName, warningMessage) {
+    $(inputName).attr('class', 'form-control');
+    $(warningMessage).attr('style', 'display:none');
+}
+
+function validateIsEmailCorrect() {
+    const email = $('#register-email').val();
+    const lowerEmail = String(email).toLowerCase();
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const isValid = re.test(lowerEmail);
+    if (!isValid) {
+        $('#signupalert').attr('style', 'display:block');
+        $('#signup-error-message').text('Adres email jest nie poprawny.');
+    }
+    return isValid;
+}
+
+function validateIsPasswordConfirmedCorrect() {
+    const password = $('#register-password').val();
+    const confirmpassword = $('#register-confirmpassword').val();
+
+    const result = password.localeCompare(confirmpassword);
+    if (result === -1) {
+        $('#signupalert').attr('style', 'display:block');
+        $('#signup-error-message').text('Hasło są różne. Spróbuj ponownie.');
+        return false;
+    }
+    return true;
+}
